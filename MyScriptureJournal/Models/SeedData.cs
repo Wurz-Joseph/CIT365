67ed3578436c10b874a61710d1dc08c2454c8780
Joseph Wurz <wur17002@byui.edu>
2020-02-22 18:40:30 -0600

    My scripture journal full commit

---
+++ MyScriptureJournal/Models/SeedData.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MyScriptureJournal.Data;
+using System;
+using System.Linq;
+
+namespace MyScriptureJournal.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MyScriptureJournalContext(serviceProvider.GetRequiredService<
+                DbContextOptions<MyScriptureJournalContext>>()))
+            {
+                if (context.Scripture.Any())
+                {
+                    return;
+                }
+                context.Scripture.AddRange(
+                    new Scripture
+                    {
+                        Book = "Matthew",
+                        Chapter = 28,
+                        Verse = 6,
+                        Note = "He is risen.",
+                        DateCreated = DateTime.Parse("2019-04-21")
+                    },
+                     new Scripture
+                     {
+                         Book = "1 Nephi",
+                         Chapter = 10,
+                         Verse = 4,
+                         Note = "Prophecy that Christ would come as Savior of the world.",
+                         DateCreated = DateTime.Parse("2019-12-25")
+                     },
+                      new Scripture
+                      {
+                          Book = "1 Corinthians",
+                          Chapter = 10,
+                          Verse = 13,
+                          Note = "God will not try/tempt you above what you can bear.",
+                          DateCreated = DateTime.Parse("2020-01-30")
+                      },
+                       new Scripture
+                       {
+                           Book = "D&C",
+                           Chapter = 121,
+                           Verse = 7,
+                           Note = "We can find peace in knowing that adversity is temporary.",
+                           DateCreated = DateTime.Parse("2019-09-12")
+                       }
+                    );
+                context.SaveChanges();
+            }
+
+        }
+    }
+}
---
+++ MyScriptureJournal/Pages/Scriptures/Index.cshtml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MyScriptureJournal.Data;
+using MyScriptureJournal.Models;
+
+namespace MyScriptureJournal
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MyScriptureJournal.Data.MyScriptureJournalContext _context;
+
+        public IndexModel(MyScriptureJournal.Data.MyScriptureJournalContext context)
+        {
+            _context = context;
+        }
+
+        public string BookSort { get; set; }
+        public string DateSort { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string CurrentFilter { get; set; }
+        public SelectList BooksFilter { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string SelectedBook { get; set; }
+        public IList<Scripture> Scriptures { get; set; }
+
+        public async Task OnGetAsync(string sortOrder, string searchString)
+        {
+            BookSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+            CurrentFilter = searchString;
+
+            IQueryable<Scripture> scriptureIQ = from s in _context.Scripture
+                                             select s;
+
+            IQueryable<string> BooksQuery = from s in _context.Scripture
+                                                select s.Book;
+
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                scriptureIQ = scriptureIQ.Where(s => s.Note.Contains(searchString));
+            }
+
+            if (!String.IsNullOrEmpty(SelectedBook))
+            {
+                scriptureIQ = scriptureIQ.Where(s => s.Book == SelectedBook);
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    scriptureIQ = scriptureIQ.OrderByDescending(s => s.Book);
+                    break;
+                case "Date":
+                    scriptureIQ = scriptureIQ.OrderBy(s => s.DateCreated);
+                    break;
+                case "date_desc":
+                    scriptureIQ = scriptureIQ.OrderByDescending(s => s.DateCreated);
+                    break;
+                default:
+                    scriptureIQ = scriptureIQ.OrderBy(s => s.Book);
+                    break;
+            }
+            var scriptures = from s in _context.Scripture
+                         select s;
+
+           
+            BooksFilter = new SelectList(await BooksQuery.Distinct().ToListAsync());
+            Scriptures = await scriptureIQ.AsNoTracking().ToListAsync();
+            
+        }
+
+    }
+}
